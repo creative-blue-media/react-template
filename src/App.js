a44e50504bfedd69698bf0695068c0bce02d131d
abm <aliff.macapinlac@gmail.com>
2019-08-16 15:47:34 -0700

    more work on webstie

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './icon.png';
 import './App.css';
 import Card from './Card.js';
+import AppRouter from './AppRouter';
 
 function App() {
   return (
     <div className="App">
+      <AppRouter />
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <section className="container">
